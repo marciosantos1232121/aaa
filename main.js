a02d442193e8e581df455d5f0e1ea84e7b08448a
marciosantos1232121 <159576070+marciosantos1232121@users.noreply.github.com>
2024-02-09 16:25:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Define uma rota para a página principal
-app.get('/', (req, res) => {
-    res.send('Bem-vindo ao painel simples em Node.js!');
-});
-
-// Inicia o servidor na porta especificada
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
+console.log('Hello, World!');
